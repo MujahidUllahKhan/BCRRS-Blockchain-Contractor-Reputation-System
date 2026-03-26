0bfec0a081001f1bb4424584e01ee89e589c1a93
Mujahid Ullah Khan Afridi <mujahidkafridi@gmail.com>
2026-03-25 22:29:49 -0600

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,24 +2,16 @@ require("@nomicfoundation/hardhat-ethers");
 require("@nomicfoundation/hardhat-chai-matchers");
 require("hardhat-gas-reporter");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
     version: "0.8.20",
     settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
+      optimizer: { enabled: true, runs: 200 },
     },
   },
-
   networks: {
-    hardhat: {
-      chainId: 31337,
-    },
+    hardhat: { chainId: 31337 },
   },
-
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
